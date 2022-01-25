230a8734267975694f5192fdf152022852bfb7c9
Kroko72 <91831185+Kroko72@users.noreply.github.com>
2022-01-25 17:21:33 +0500

    Add files via upload

--- game2.py
+++ game2.py
@@ -26,9 +26,56 @@ def where_to_go(go_left=False, go_right=False, go_forward=False, back1=False, f1
     back_left = b2
 
 
+# Экран победы
+def win_screen():
+    global attempt_time, attempts  # Время попытки из главного цикла и количество попыток
+    # Лучшее время прохождения, считанное из файла
+    best_time_file = open('data/best_time.txt', 'r')
+    time_from_file = best_time_file.read().split(":")
+
+    temp_time = attempt_time.split(":")  # Временная локальная переменная, хранящая время попытки в виде списка
+    beated = False  # Побит ли рекорд
+
+    # Перевод времени попыток в секунды для сравнения
+    seconds_from_file = (int(time_from_file[0]) * 3600 + int(time_from_file[1]) * 60 + int(time_from_file[2]))
+    seconds_from_attempt = (int(temp_time[0]) * 3600 + int(temp_time[1]) * 60 + int(temp_time[2]))
+
+    # Сравнение времени прохождения, и если лучшее время побито, то оно записывается в файл с лучшим временем
+    if seconds_from_attempt < seconds_from_file:
+        file = open('data/best_time.txt', 'w')
+        file.write(attempt_time)
+        beated = True
+
+    fon = pygame.transform.scale(load_image('win_screen.png'), (width, height))  # Фон
+    while True:
+        screen.blit(fon, (0, 0))  # Заполнение фона начального экрана
+        # Если рекорд побит, то добавляется соответсвующая надпись
+        if beated is True:
+            print_text(f'Вы побили свой рекорд!', 650, 320)
+        print_text(f'Время вашей попытки: {attempt_time}', 5, 7)  # Текст с временем попытки
+        # Текст с количеством попыток, затраченных на прохождение
+        print_text(f'На прохождение ушло {attempts} попыток', 5, 40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                # Проверка нажатия на кнопку EXIT
+                if 617 < event.pos[0] < 918 and 358 < event.pos[1] < 439:
+                    terminate()  # Окончание работы
+        # Замена курсора
+        if pygame.mouse.get_focused():
+            screen.blit(cursor, pygame.mouse.get_pos())
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
 # Функция проигрыша (смерти героя)
 def lose_screen():
-    global attempt_time  # Время попытки из главного цикла
+    global attempt_time, death_sound  # Время попытки из главного цикла и звук смерти
+
+    # Проигрывание звука смерти
+    death_sound.play()
+
     fon = pygame.transform.scale(load_image('lose_screen.png'), (width, height))  # Фон
     while True:
         screen.blit(fon, (0, 0))  # Заполнение фона начального экрана
@@ -119,7 +166,7 @@ def draw_player():
 def pause():
     pause_time = time.time()  # Время начала паузы
     paused = True
-    print_text('Press Enter to continue or press R to restart', 580, 385)  # Добавление текста на экран
+    print_text('Нажмите Enter для продолжения или R для рестарта', 520, 385)  # Добавление текста на экран
     while paused:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -165,6 +212,7 @@ def load_image(name, colorkey=None):
 # Функция рестарта
 def restart_level():
     global where_x, where_y, arrow_group, boss_group, attempt_start_time, animCount, roll, Level, Golem_attack_group
+    global Crystal_attack_group, attempts
     # Обновление координат
     where_x = sprite.rect.x = 770
     where_y = sprite.rect.y = 660
@@ -172,28 +220,33 @@ def restart_level():
     boss_group.empty()
     boss_group.add(Knight())
     Golem_attack_group.empty()
+    Crystal_attack_group.empty()
     Level = 1
+    attempts += 1
 
     # Обновление анимации
     animCount = 0
     roll = False
     attempt_start_time = time.time()  # Обнуление времени
-    arrow_group = pygame.sprite.Group()  # Обновление стрел
+    arrow_group.empty()  # Обновление стрел
 
 
 # Начальное окно
 def start_screen():
-    best_time = "10"  # Лучшее время прохождения
     fon = pygame.transform.scale(load_image('fon.png'), (width, height))
+
+    best_time_file = open('data/best_time.txt', 'r').read()  # Лучшее время прохождения (считано из файла)
+
     while True:
         screen.blit(fon, (0, 0))
-        print_text(f'Ваш рекорд: {best_time}', 5, 7)  # Добавление лучшего времени прохождения на экран
+        print_text(f'Ваш рекорд: {best_time_file}', 5, 7)  # Добавление лучшего времени прохождения на экран
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 # Проверка нажатия на кнопку START
                 if 644 < event.pos[0] < 933 and 296 < event.pos[1] < 371:
+                    start_sound.play()  # Звук начала игры
                     return  # начинаем игру
                 # Проверка нажатия на кнопку EXIT
                 elif 640 < event.pos[0] < 933 and 394 < event.pos[1] < 469:
@@ -283,7 +336,7 @@ class Knight(pygame.sprite.Sprite):
         self.dash_was = False  # Был ли рывок
         self.much_wait = 0  # Задержка (в итерациях) после рывка
         self.vel_x, self.vel_y = 0, 0
-        self.rect = self.image.get_rect(center=(800, 170))
+        self.rect = self.image.get_rect(center=(780, 300))
         self.angle = math.atan2(where_y - self.rect.y, where_x - self.rect.x)  # Направление движения
 
     def update(self):
@@ -296,8 +349,8 @@ class Knight(pygame.sprite.Sprite):
         else:
             self.animation_count += 1  # Смена картинок анимации
 
-        # Раз в пять секунд рывок
-        if int(time.time() - seconds) % 5 == 0:
+        # Раз в три секунд рывок
+        if int(time.time() - seconds) % 3 == 0:
             self.speed = 10
             self.dash_count += 1
             self.dash_was = False
@@ -318,14 +371,14 @@ class Knight(pygame.sprite.Sprite):
         # Если рыцарь идёт влево, то его картинка отражается
         if self.vel_x < 0:
             self.image = pygame.transform.flip(self.image, True, False)
-        if 50 < self.rect.x < 1500 and 10 < self.rect.y < 730 and self.must_wait is False:
-            if 50 < (self.rect.x + int(self.vel_x)) < 1500 and 50 < (self.rect.y + int(self.vel_y)) < 730:
+        if 59 < self.rect.x < 1440 and 100 < self.rect.y < 530 and self.must_wait is False:
+            if 59 < (self.rect.x + int(self.vel_x)) < 1440 and 100 < (self.rect.y + int(self.vel_y)) < 530:
                 self.rect.x += int(self.vel_x)
                 self.rect.y += int(self.vel_y)
 
         # Задержка после рывка в итерациях
         elif self.must_wait is True:
-            if self.much_wait == 30:
+            if self.much_wait == 20:
                 self.must_wait = False
                 self.much_wait = 0
             else:
@@ -404,7 +457,7 @@ class Golem(pygame.sprite.Sprite):
             # Если во время пульсации попасть голему в глаз, то он умирает
             if pygame.sprite.spritecollideany(self, arrow_group):
                 for i in arrow_group:
-                    if self.rect.x + 100 < i.rect.x < self.rect.x + 195 and self.rect.y < i.rect.y < self.rect.y + 150:
+                    if self.rect.x + 50 < i.rect.x < self.rect.x + 195 and self.rect.y < i.rect.y < self.rect.y + 100:
                         win_status = True
                         self.kill()
             if self.pulse_count + 1 == 8:
@@ -428,8 +481,8 @@ class Golem(pygame.sprite.Sprite):
             else:
                 self.animation_count += 1  # Смена картинок анимации
 
-        # Раз в 50 итераций выстрел
-        if self.shoot_iterations == 50:
+        # Раз в 30 итераций выстрел
+        if self.shoot_iterations == 30:
             self.shoot_count += 1
             # Смещенение снаряда голема если он идёт влево, чтобы выглядело так, что он стреляет из руки
             if self.vel_x > 0:
@@ -487,6 +540,102 @@ class GolemAttack(pygame.sprite.Sprite):
             lose_screen()  # Запуск экрана проигрыша
 
 
+# Класс босса кристала
+class Crystal(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        # Цвет заднего фона и загрузка изображения кристалла с убраным задним фоном
+        color = (127, 146, 255)
+        self.image = pygame.transform.scale(load_image("crystal.png", color), (100, 240))
+
+        # Обычная анимация кристалла
+        self.animation_images = [pygame.transform.scale(load_image("crystal.png", color), (100, 240)),
+                                 pygame.transform.scale(load_image("crystal1.png", color), (100, 240)),
+                                 pygame.transform.scale(load_image("crystal2.png", color), (100, 240)),
+                                 pygame.transform.scale(load_image("crystal3.png", color), (100, 240)),
+                                 pygame.transform.scale(load_image("crystal4.png", color), (100, 240)),
+                                 pygame.transform.scale(load_image("crystal5.png", color), (100, 240))]
+        # Анимация стрельбы
+        self.shoot_images = [pygame.transform.scale(load_image("crystal6.png", color), (100, 240)),
+                             pygame.transform.scale(load_image("crystal7.png", color), (100, 240)),
+                             pygame.transform.scale(load_image("crystal8.png", color), (100, 240))]
+
+        # Время появления босса
+        self.creation_time = time.time()
+        self.shoot_count = 0  # Счётчик для анимации стрельбы
+        self.animation_count = 0  # Счётчик для обычной анимации
+        self.shoot_time = time.time()  # Время последнего выстрела
+        self.rect = self.image.get_rect(center=(800, 300))
+
+    def update(self):
+        global Crystal_attack_group, win_status
+        # Каждые две секунды выстрел
+        if (time.time() - self.shoot_time) > 0.5:
+            Crystal_attack_group.add(CrystalAttack(self.rect.x, self.rect.y))
+            self.shoot_time = time.time()
+            self.shoot_count += 1
+
+        # Анимация выстрелов
+        if self.shoot_count != 0:
+            self.image = self.shoot_images[self.shoot_count // 6]
+            if self.shoot_count + 1 == 18:
+                self.shoot_count = 0
+            else:
+                self.shoot_count += 1
+
+            # Во время выстрела кристал можно убить, если с момента создания босса прошло больше трёх
+            if pygame.sprite.spritecollideany(self, arrow_group) and (time.time() - self.creation_time) > 3:
+                win_status = True  # Анимация танца
+                self.kill()
+
+        # Обычная анимация
+        else:
+            self.image = self.animation_images[self.animation_count // 6]
+            if self.animation_count + 1 == 34:
+                self.animation_count = 0  # Начало анимации заново
+            else:
+                self.animation_count += 1  # Смена картинок анимации
+
+        # Если босс задел героя, то игра проиграна
+        if pygame.sprite.collide_mask(sprite, self):
+            lose_screen()  # Запуск экрана проигрыша
+
+
+# Класс лазеров кристала
+class CrystalAttack(pygame.sprite.Sprite):
+    def __init__(self, crystal_x, crystal_y):
+        super().__init__()
+        self.image = pygame.transform.scale(load_image("laser.png", (116, 154, 212)), (100, 69))
+        self.speed = 6  # Начальная скорость
+        self.vel_x = self.vel_y = 0  # Скорость по оси икс и игрик
+        self.rect = self.image.get_rect(center=(crystal_x + 60, crystal_y + 10))
+        self.angle = math.atan2(where_y - self.rect.y, where_x - self.rect.x)  # Угол наклона (направление)
+
+        # Угол поворота картинки снаряда и разворачивание его в направлени выпускания
+        rel_x, rel_y = crystal_x + 60 - where_x, crystal_y + 10 - where_y
+        angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+        self.image = pygame.transform.rotate(self.image, angle)
+
+    def update(self):
+        self.angle = math.atan2(where_y - self.rect.y, where_x - self.rect.x)  # Угол наклона (направление)
+        self.vel_x = math.cos(self.angle) * self.speed  # Скорость по иксу
+        self.vel_y = math.sin(self.angle) * self.speed  # Скорость по игрику
+        if self.rect.x < 1500 and self.rect.y < 760:
+            self.rect.x += int(self.vel_x)
+            self.rect.y += int(self.vel_y)
+        else:
+            self.kill()
+
+        # Если снаряд задел героя, то игра проиграна
+        if pygame.sprite.collide_mask(sprite, self):
+            lose_screen()  # Запуск экрана проигрыша
+
+        # Если стрела попала в снаряд, он взрывается и исчезает, а стрела возвращается назад
+        if pygame.sprite.spritecollideany(self, arrow_group):
+            self.kill()
+            arrow_group.empty()
+
+
 # Инициализация пайгейма
 pygame.init()
 
@@ -495,8 +644,15 @@ pygame.mixer.music.load("data/menu.ogg")
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(-1)
 
-# Группа атак босса голема
+arrow_sound = pygame.mixer.Sound("data/arrow_shoot.ogg")  # Звук выстрела
+death_sound = pygame.mixer.Sound("data/death.ogg")  # Звук смерти главного героя
+start_sound = pygame.mixer.Sound("data/StartGame.ogg")  # Звук начала игры
+
+# Группа снарядов босса голема
 Golem_attack_group = pygame.sprite.Group()
+
+# Группа лазеров босса кристала
+Crystal_attack_group = pygame.sprite.Group()
 
 # Создание главного окна
 size = width, height = 1530, 790
@@ -670,6 +826,9 @@ hero_group.add(sprite)
 # Группа стрелы
 arrow_group = pygame.sprite.Group()
 
+# Счётчик попыток
+attempts = 1
+
 # Запуск начального окна
 start_screen()
 
@@ -684,7 +843,7 @@ seconds = time.time()
 attempt_start_time = time.time()
 
 # Задний фон игры
-bg = load_image("1.png")
+bg = load_image("bg_test.png")
 
 # Переменные необходимые для анимации
 right, left, forward, back, forward_right, forward_left, back_right, back_left = False, False, False, False, False,\
@@ -744,13 +903,14 @@ while running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             # Если стрела не выпущена, то игрок может выстрелить
             if event.button == 1 and len(arrow_group) == 0:
+                arrow_sound.play()
                 x, y = pygame.mouse.get_pos()  # Координаты курсора
-                arrow_group = pygame.sprite.Group()
+                arrow_group.empty()
                 arrow_group.add(Arrow(sprite.rect.x, sprite.rect.y, x, y))
                 go_back = False
             # Если стрела выпущена, то она притягивается к игроку
             elif event.button == 3 and len(arrow_group) == 1:
-                arrow_group = pygame.sprite.Group()
+                arrow_group.empty()
                 arrow_group.add(ArrowBack(sprite.rect.x, sprite.rect.y, old_arrow_x, old_arrow_y))
                 go_back = True
 
@@ -763,43 +923,52 @@ while running:
 
     # Перемещение персонажа
     keys = pygame.key.get_pressed()  # Список с состоянием(нажата или нет) каждой клавишы
+    # Перемещение влево вверх
     if keys[pygame.K_w] and keys[pygame.K_a]:
-        if sprite.rect.y > 30 and sprite.rect.x > 30:
+        if sprite.rect.y > 200 and sprite.rect.x > 60:
             sprite.rect.y -= hero_speed
             sprite.rect.x -= hero_speed
             where_to_go(f2=True)
+    # Перемещение вправо вверх
     elif keys[pygame.K_w] and keys[pygame.K_d]:
-        if sprite.rect.y > 30 and sprite.rect.x < 1465:
+        if sprite.rect.y > 200 and sprite.rect.x < 1465:
             sprite.rect.y -= hero_speed
             sprite.rect.x += hero_speed
             where_to_go(f1=True)
+    # Перемещение влево вниз
     elif keys[pygame.K_s] and keys[pygame.K_a]:
-        if sprite.rect.y < 720 and sprite.rect.x > 30:
+        if sprite.rect.y < 680 and sprite.rect.x > 60:
             sprite.rect.y += hero_speed
             sprite.rect.x -= hero_speed
             where_to_go(b2=True)
+    # Перемещение вправо вниз
     elif keys[pygame.K_s] and keys[pygame.K_d]:
-        if sprite.rect.y < 720 and sprite.rect.x < 1465:
+        if sprite.rect.y < 680 and sprite.rect.x < 1465:
             sprite.rect.y += hero_speed
             sprite.rect.x += hero_speed
             where_to_go(b1=True)
+    # Перемещение влево
     elif keys[pygame.K_a]:
-        if sprite.rect.x > 30:
+        if sprite.rect.x > 60:
             sprite.rect.x -= hero_speed
             where_to_go(go_left=True)
+    # Перемещение вправо
     elif keys[pygame.K_d]:
         if sprite.rect.x < 1465:
             sprite.rect.x += hero_speed
             where_to_go(go_right=True)
+    # Перемещение вверх
     elif keys[pygame.K_w]:
-        if sprite.rect.y > 30:
+        if sprite.rect.y > 200:
             sprite.rect.y -= hero_speed
             where_to_go(go_forward=True)
+    # Перемещение вниз
     elif keys[pygame.K_s]:
-        if sprite.rect.y < 720:
+        if sprite.rect.y < 680:
             sprite.rect.y += hero_speed
             where_to_go(back1=True)
-    elif win_status is False:  # Если игрок не победил, то будет картинка стоящего на месте героя
+    # Если игрок не победил, то будет картинка стоящего на месте героя
+    elif win_status is False:
         where_to_go()
         animCount = 0
 
@@ -821,12 +990,17 @@ while running:
     print_text(attempt_time, 10, 10)  # Время попытки
     draw_player()  # Анимации героя (изменение его картинок)
     hero_group.draw(screen)  # Отрисовка героя
-    arrow_group.draw(screen)  # Отрисовка стрела
+    arrow_group.draw(screen)  # Отрисовка стрелы
     boss_group.draw(screen)  # Отрисовка босса
 
+    # Если второй уровень, то обновление и отрисовка снарядов голема
     if Level == 2:
         Golem_attack_group.update()
         Golem_attack_group.draw(screen)
+
+    if Level == 3:
+        Crystal_attack_group.update()
+        Crystal_attack_group.draw(screen)
 
     # Замена курсора
     if pygame.mouse.get_focused():
@@ -834,15 +1008,24 @@ while running:
 
     # Окно перехода на следующий уровень
     if win_status is True:
-        print_text('Нажмите Enter для перехода на следующий уровень', 580, 385)  # Добавление текста на экран
-        if keys[pygame.K_RETURN]:  # Проверка на нажатие кнопки Enter
-            boss_group.add(Golem())  # Добавление следующего басса
-            Level += 1
+        if Level == 1 or Level == 2:
+            print_text('Нажмите Enter для перехода на следующий уровень', 520, 385)  # Добавление текста на экран
+            if keys[pygame.K_RETURN]:  # Проверка на нажатие кнопки Enter
+                if Level == 1:
+                    boss_group.add(Golem())  # Добавление следующего басса голема
+                if Level == 2:
+                    boss_group.add(Crystal())  # Добавление следующего босса кристала
+                Level += 1
 
-            # Обновление координат
-            where_x = sprite.rect.x = 770
-            where_y = sprite.rect.y = 660
-            win_status = False
+                # Обновление координат
+                where_x = sprite.rect.x = 770
+                where_y = sprite.rect.y = 660
+                win_status = False
+                arrow_group.empty()  # Обновление стрел
+
+        # Если игры пройдёна, то выходит экран победы
+        elif Level == 3:
+            win_screen()
 
     pygame.display.flip()  # Обновление кадра
     clock.tick(FPS)  # Ограничение частоты кадров
